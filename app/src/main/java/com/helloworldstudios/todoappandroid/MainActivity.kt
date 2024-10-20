c7cf2556c1e09dcbcdbdf0b3e5bc5967dff8e1ea
Enes CAN <enesscann98@gmail.com>
2024-10-20 05:54:44 +0300

    fixup! add viewBinding imported

--- app/src/main/java/com/helloworldstudios/todoappandroid/MainActivity.kt
+++ app/src/main/java/com/helloworldstudios/todoappandroid/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.helloworldstudios.todoappandroid
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.helloworldstudios.todoappandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
